aab3f7525e337300e2b16e3a2b43311963c73732
Edwin <edmboche141@gmail.com>
2024-12-13 09:04:48 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 function addingEventListener() {
-}
+    const input = document.getElementById('button');
+    
+    // Add a click event listener to the button
+    input.addEventListener('click', function() {
+      console.log('Button clicked!');
+    });
+  }
+ 
